824ff9356a22d7b72ecd898a85bf087230387988
FarazBookbinder <muaaviya26@gmail.com>
2024-06-02 16:21:20 +0530

    first commit 02/06/2024

---
+++ Python_learning_practice/variable_practice_day_1/Simple_equation_programs.py
@@ -0,0 +1,10 @@
+a = 10
+b = 20
+Sum = a + b
+diff = b - a
+mult = a * b
+divide = a / b
+print("Sum of a and b is:-", Sum)
+print("Difference of a and b is:-", diff)
+print("Multiplication of a + b is:-", mult)
+print("Divide of a and b is:-", divide)
