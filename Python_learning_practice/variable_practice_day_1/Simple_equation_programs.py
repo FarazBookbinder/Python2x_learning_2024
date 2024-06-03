77385987351244f38c9b5dab1df99d96231f3641
FarazBookbinder <muaaviya26@gmail.com>
2024-06-03 23:50:00 +0530

    commit 03/06/2024

--- Python_learning_practice/variable_practice_day_1/Simple_equation_programs.py
+++ Python_learning_practice/variable_practice_day_1/Simple_equation_programs.py
@@ -1,10 +1,14 @@
-a = 10
-b = 20
+a = 5
+b = 2
 Sum = a + b
 diff = b - a
 mult = a * b
 divide = a / b
+modulo_reminder = a % b
+power_operator = a ** b
 print("Sum of a and b is:-", Sum)
 print("Difference of a and b is:-", diff)
 print("Multiplication of a + b is:-", mult)
 print("Divide of a and b is:-", divide)
+print("Your remainder value is:-", modulo_reminder)
+print("Power of a^b is:-", power_operator)
