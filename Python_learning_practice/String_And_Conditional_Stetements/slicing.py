521a702ec51fb547efdda39bfd637386907e0d02
FarazBookbinder <muaaviya26@gmail.com>
2024-10-22 22:59:50 +0530

    Commit again on 22/10/2024

---
+++ Python_learning_practice/String_And_Conditional_Stetements/slicing.py
@@ -0,0 +1,11 @@
+# slicing is used in python for the part of the string
+
+# slicing is strt with 0 and end with -1 of the entered indexing value.
+
+# value_1 = "Faraz Bookbinder"
+# print(value_1[1:4])
+
+value_1 = "Faraz Bookbinder"
+print(value_1[: 4])
+print(value_1[7:])
+
