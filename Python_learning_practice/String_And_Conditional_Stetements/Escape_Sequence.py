a932ea17fecff1a79bb4f3a7016300841c958acd
FarazBookbinder <muaaviya26@gmail.com>
2024-10-22 22:37:11 +0530

    Commit on 22-10-2024

---
+++ Python_learning_practice/String_And_Conditional_Stetements/Escape_Sequence.py
@@ -0,0 +1,10 @@
+#String with the escape sequence.
+
+my_string = "I am Muaaviya Bookbinder.\nMy wife name is Afrin.\nMy Son name is Omair"
+my_updated_string = my_string
+print(my_updated_string)
+
+my_string = "I am Muaaviya Bookbinder\tMy wife name is Afrin\t My Son name is Omair"
+my_updated_string = my_string
+print(my_updated_string)
+
