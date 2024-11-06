e51c5c2e65089b58a4bacb82d0171400fe1ad4ef
FarazBookbinder <muaaviya26@gmail.com>
2024-11-07 01:15:46 +0530

    Commit again on 06/11/2024

--- Python_learning_practice/loops_practice_again/while_loop_concept.py
+++ Python_learning_practice/loops_practice_again/while_loop_concept.py
@@ -2,3 +2,48 @@
 # while condition:
     # Whatever work you need to write here.
 
+# count = 1
+# while count <= 5:
+#     print("Hello")
+#     count += 1
+
+
+# count = 5
+# while count >= 1:
+#     print(count)
+#     count -= 1
+
+# WAP print number from 1 to 100
+# number = 1
+# while number <= 100:
+#     print(number)
+#     number += 1
+
+# WAP print number from 1 to 100
+# number_new = 100
+# while number_new >= 1:
+#     print(number_new)
+#     number_new -= 1
+
+# WAP print the multiplication table of number n
+# num = int(input("Please enter the number:-"))
+# i = 1
+# while i <= 10:
+#     print(f"{num} {"*"} {i} {"="}", num * i)
+#     i += 1
+
+# print the element of the following list
+# my_list = [1, 4, 8, 9, 12, 78, 99, 34, 45, 88]
+# while len(my_list)-1:
+#     print(my_list)
+#     break
+#Better way
+my_list = [1, 4, 8, 9, 12, 78, 99, 34, 45, 88]
+index = 0
+while index < len(my_list):
+    print(my_list[index])
+    index += 1
+
+
+
+
