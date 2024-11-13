318781307d74b35d9dd679d94e94cabd8d82db25
FarazBookbinder <muaaviya26@gmail.com>
2024-11-14 01:12:21 +0530

    Commit again on 14/11/2024

--- Python_learning_practice/loops_practice_again/for_loops/for_loops_range_function.py
+++ Python_learning_practice/loops_practice_again/for_loops/for_loops_range_function.py
@@ -6,5 +6,43 @@
 # for u in range(20):
 #     print(u)
 
-for u in range(1, 20, 2):
-    print(u)
+# for u in range(2, 20, 2):
+#     print(u)
+
+# WAP to print a number from 1 too 100
+# for i in range(1, 101):
+#     print(i)
+
+# WAP to print a number from 1 too 100
+# for i in range(100, 0, -1):
+#     print(i)
+
+# WAP to print a multiplication table for number n
+# num_input = int(input("Please enter the number"))
+# for i in range(1, 11):
+#     print(num_input, "*", i, "=", i * num_input)
+
+# WAP to print a sum of first n numbers
+# n = 12
+# sum = 0
+# for i in range(1, n+1):
+#     sum += i
+# print("Total is:-", sum)
+
+# Above Program using while loop
+# n = 12
+# sum = 0
+# i = 1
+# while i <= n:
+#     sum += i
+#     i += 1
+# print("Total is:-", sum)
+
+# WAP to print a factorial of n number. using a while loop
+n = 4
+fact = 1
+i = 1
+while i <= n:
+    fact = fact * i
+    i += 1
+print("Factorail is:-", fact)
